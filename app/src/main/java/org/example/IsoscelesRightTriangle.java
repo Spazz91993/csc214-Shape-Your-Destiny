047fca5a594b4f9386d052af7c7cecd7a04975c7
Nick Spizzirri <Nick.spizzirri91993@gmail.com>
2025-02-23 04:29:56 -0600

    upgrade
    realized i didnt need to upgrade methods on isosceles triangle

--- app/src/main/java/org/example/IsoscelesRightTriangle.java
+++ app/src/main/java/org/example/IsoscelesRightTriangle.java
@@ -7,17 +7,6 @@ public class IsoscelesRightTriangle extends RightTriangle {
     }
 
     @Override
-    public double getArea() {
-        return super.getArea();
-    }
-
-    @Override
-    public double getPerimeter() {
-        double hypotenuse = Math.sqrt(2) * super.getPerimeter();
-        return super.getPerimeter();
-    }
-
-    @Override
     public String getShape() {
         return "Isosceles Right Triangle.";
     }
